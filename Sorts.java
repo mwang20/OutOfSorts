787dee5e3686b735cbc70685c06d237b9c507880
mwang20 <mwang20@stuy.edu>
2020-12-17 23:39:41 -0500

    Used a while loop to create the bubbleSort method and it loops through the values and swaps the value if the value of the index is higher than the value of the index+1.

--- Sorts.java
+++ Sorts.java
@@ -1,9 +1,17 @@
 public class Sorts{
   public static void bubbleSort(int[] data){
     int n = data.length;
-    boolean swap; 
-    for (int i = 0; i < n; i++){
-
+    boolean swap = true;
+    while (swap){
+      swap = false;
+      for (int i = 0; i < n - 1; i++){
+        if (data[i] > data[i + 1]){
+          int swapped = data[i];
+          data[i] = data[i + 1];
+          data[i+1] = swapped;
+          swap = true;
+        }
+      }
     }
   }
 }
