a2a373532c407cb31d2165bd6ac35267c5a83cbe
mwang20 <mwang20@stuy.edu>
2020-12-18 00:20:01 -0500

    Created the arrays I will be using to test my bubbleSort method.

--- Tester.java
+++ Tester.java
@@ -1,6 +1,9 @@
 public class Tester extends Sorts{
   public static boolean DEBUG = true;
   public static void main(String[]args){
-    
+    int[] A = new int[]{4, 2, 5, 3, 6, 10, 1, 7, 9, 8};
+    int[] B = new int[]{1, 3, 4, 2, 7, 5, 9, 10, 6, 8};
+    int[] C = new int[]{7, 8, 10, 1, 3, 2, 6, 5, 9, 4};
+    int[] real = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   }
 }
